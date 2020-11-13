8f9506b14910bb4b6fcf4ed3f783046ea5afe644
Henri des Rieux <desrieux.henri@gmail.com>
2020-11-13 12:29:37 +0100

    test

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
+
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -23,12 +24,13 @@ module RailsMisterCocktail
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
+    config.assets.debug = false
+    config.assets.check_precompiled_asset = false
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
