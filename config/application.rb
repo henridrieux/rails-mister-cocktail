60e291d8a220b3297ed4b8e8c01bea3970e8ab5e
Henri des Rieux <desrieux.henri@gmail.com>
2020-11-13 12:54:05 +0100

    exo

--- config/application.rb
+++ config/application.rb
@@ -29,8 +29,7 @@ module RailsMisterCocktail
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.assets.debug = false
-    config.assets.check_precompiled_asset = false
+
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
